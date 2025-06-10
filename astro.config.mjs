9778d88e5e2cecbba04fa92a4cdd1bcd05344da7
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2025-06-09 23:05:16 -0600

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import storyblok from '@storyblok/astro'
 import { loadEnv } from 'vite'
 import tailwind from '@astrojs/tailwind'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import netlify from '@astrojs/netlify';
 const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
@@ -26,10 +28,13 @@ export default defineConfig({
     }),
     tailwind(),
   ],
+
   vite: {
     plugins: [basicSsl()],
     server: {
       https: true,
     },
   },
+
+  adapter: netlify(),
 })
